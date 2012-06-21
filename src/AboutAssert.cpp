159eb35a513ff76e359278dc9a31284eabb61294
B Kleinen <kleinen@infrastructure.de>
2012-06-21 10:08:05 +0200

    removed not compiled cast

--- src/AboutAssert.cpp
+++ src/AboutAssert.cpp
@@ -8,7 +8,7 @@ void aboutAssertParameterTypes(){
 	expectThat("however, if they are not the same, expected is tried to be cast to actual's type.",3.0,3);
 	expectThat("truncating information when casting to narrower type",3.1,3);
 	expectThatNot("and not if casted to a wider type",3,3.1);
-	expectThatNot("you won't get an Error for bad types","expected",42);
+	//expectThatNot("you won't get an Error for bad types - apparently you do on some compilers","expected",42);
 	//this causes a crash
 	//expectThat("this is the error message you get when the cast failed",42,"expected");
 	expectThat("thus, you can also compare c-strings with cpp-strings","cstring",string("cstring"));
